eca3984c08d6cf1f981941bdb9764c751b682c34
waneyok <waneyok@yandex.ru>
2026-02-02 20:21:46 +0300

    первое задание

---
+++ WPractice/WPractice_4/WPractice_4/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+namespace WPractice_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
